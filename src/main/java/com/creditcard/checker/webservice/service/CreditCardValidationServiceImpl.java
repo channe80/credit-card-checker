020e7c9b9699594198f5a4e2aff2cb05470a53e4
anne <atorres@concordinfiniti.com>
2021-06-25 08:54:17 +1000

    Add regex patterns and format checks for amex, discover and mastercard

--- src/main/java/com/creditcard/checker/webservice/service/CreditCardValidationServiceImpl.java
+++ src/main/java/com/creditcard/checker/webservice/service/CreditCardValidationServiceImpl.java
@@ -8,27 +8,58 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Service
-public class CreditCardValidationServiceImpl  implements CreditCardValidationService {
+public class CreditCardValidationServiceImpl implements CreditCardValidationService {
 
+    //AMEX Card begins with 34 or 37, number length of 15
+    private static Pattern AMEX_PATTERN = Pattern.compile("^3[47][0-9]{13}$");
+    //Discover Card begins with 6011, number length of 16
+    private static Pattern DISCOVER_PATTERN = Pattern.compile("^6011[0-9]{12}$");
+    //Mastercard begins with 51 to 55, number length of 16
+    private static Pattern MASTERCARD_PATTERN = Pattern.compile("^5[1-5][0-9]{14}$");
     //Visa begins with 4, number length of 13 or 16
-    private static Pattern visa = Pattern.compile("^4[0-9]{12}(?:[0-9]{3})?$");
+    private static Pattern VISA_PATTERN = Pattern.compile("^4[0-9]{12}(?:[0-9]{3})?$");
 
     @Override
     public CardType getCardType(String cardNumber) {
         if (null == cardNumber || cardNumber.isEmpty() || cardNumber.isBlank()) {
             return CardType.Unknown;
         }
-        //remove white spaces
+
         String cardNumberTrimmed = StringUtils.trimAllWhitespace(cardNumber);
 
+        if (isAmexCardFormat(cardNumberTrimmed)) {
+            return CardType.AMEX;
+        }
+        if (isDiscoverCardFormat(cardNumberTrimmed)) {
+            return CardType.Discover;
+        }
+        if (isMasterCardFormat(cardNumberTrimmed)) {
+            return CardType.MasterCard;
+        }
         if (isVisaCardFormat(cardNumberTrimmed)) {
             return CardType.Visa;
         }
+
         return CardType.Unknown;
     }
 
+    private boolean isAmexCardFormat(String cardNumber) {
+        Matcher m = AMEX_PATTERN.matcher(cardNumber);
+        return m.matches();
+    }
+
+    private boolean isDiscoverCardFormat(String cardNumber) {
+        Matcher m = DISCOVER_PATTERN.matcher(cardNumber);
+        return m.matches();
+    }
+
+    private boolean isMasterCardFormat(String cardNumber) {
+        Matcher m = MASTERCARD_PATTERN.matcher(cardNumber);
+        return m.matches();
+    }
+
     private boolean isVisaCardFormat(String cardNumber) {
-        Matcher m = visa.matcher(cardNumber);
+        Matcher m = VISA_PATTERN.matcher(cardNumber);
         return m.matches();
     }
 
--- src/test/java/com/creditcard/checker/webservice/service/CreditCardValidationServiceImplTest.java
+++ src/test/java/com/creditcard/checker/webservice/service/CreditCardValidationServiceImplTest.java
@@ -19,7 +19,106 @@ class CreditCardValidationServiceImplTest {
     }
 
     @Test
-    void getCardType_validVisa() {
+    void getCardType_validAmexFormat() {
+        CreditCardValidationService cardValidationService = new CreditCardValidationServiceImpl();
+
+        //valid amex format
+        String amexCardNumber34 = "348282246310005";
+        Assertions.assertEquals(CardType.AMEX, cardValidationService.getCardType(amexCardNumber34));
+
+        String amexCardNumber37 = "378282246310005";
+        Assertions.assertEquals(CardType.AMEX, cardValidationService.getCardType(amexCardNumber37));
+
+        //non amex format - does not start with 34 / 37
+        String doesNotStartwith34 = "448282246310005";
+        Assertions.assertNotEquals(CardType.AMEX, cardValidationService.getCardType(doesNotStartwith34));
+
+        String doesNotStartwith34b = "358282246310005";
+        Assertions.assertNotEquals(CardType.AMEX, cardValidationService.getCardType(doesNotStartwith34b));
+
+        //non amex format - length < 16
+        String lengthLessThan13 = "3411111111111";
+        Assertions.assertNotEquals(CardType.AMEX, cardValidationService.getCardType(lengthLessThan13));
+
+        //non amex format - length > 16
+        String lengthMoreThan16 = "3411111111111122222";
+        Assertions.assertNotEquals(CardType.AMEX, cardValidationService.getCardType(lengthMoreThan16));
+
+        //contains non-numeric
+        String hasNonNumeric = "34abc1111111111";
+        Assertions.assertEquals(CardType.Unknown, cardValidationService.getCardType(hasNonNumeric));
+    }
+
+    @Test
+    void getCardType_validDiscoverFormat() {
+        CreditCardValidationService cardValidationService = new CreditCardValidationServiceImpl();
+
+        //valid discover format
+        String amexCardNumber = "6011822463100050";
+        Assertions.assertEquals(CardType.Discover, cardValidationService.getCardType(amexCardNumber));
+
+        //non discover format - does not start with 6011
+        String doesNotStartwith6011 = "7011822463100050";
+        Assertions.assertNotEquals(CardType.Discover, cardValidationService.getCardType(doesNotStartwith6011));
+
+        //non discover format - length < 16
+        String lengthLessThan13 = "6011111111111";
+        Assertions.assertNotEquals(CardType.Discover, cardValidationService.getCardType(lengthLessThan13));
+
+        //non visa format - length > 16
+        String lengthMoreThan16 = "6011111111111122222";
+        Assertions.assertNotEquals(CardType.Discover, cardValidationService.getCardType(lengthMoreThan16));
+
+        //contains non-numeric
+        String hasNonNumeric = "6011abc1111111111";
+        Assertions.assertEquals(CardType.Unknown, cardValidationService.getCardType(hasNonNumeric));
+    }
+
+    @Test
+    void getCardType_validMasterCardFormat() {
+        CreditCardValidationService cardValidationService = new CreditCardValidationServiceImpl();
+
+        //valid mastercard format
+        String masterCardNumber51 = "5105105105105100";
+        Assertions.assertEquals(CardType.MasterCard, cardValidationService.getCardType(masterCardNumber51));
+
+        String masterCardNumber52 = "5205105105105100";
+        Assertions.assertEquals(CardType.MasterCard, cardValidationService.getCardType(masterCardNumber52));
+
+        String masterCardNumber53 = "5305105105105100";
+        Assertions.assertEquals(CardType.MasterCard, cardValidationService.getCardType(masterCardNumber53));
+
+        String masterCardNumber54 = "5405105105105100";
+        Assertions.assertEquals(CardType.MasterCard, cardValidationService.getCardType(masterCardNumber54));
+
+        String masterCardNumber55 = "5505105105105100";
+        Assertions.assertEquals(CardType.MasterCard, cardValidationService.getCardType(masterCardNumber55));
+
+        String masterCardNumber51WithSpace = "5105 1051 0510 5106";
+        Assertions.assertEquals(CardType.MasterCard, cardValidationService.getCardType(masterCardNumber51WithSpace));
+
+        //non mastercard format - does not start with 51-55
+        String doesNotStartwith51_55 = "6505105105105100";
+        Assertions.assertNotEquals(CardType.MasterCard, cardValidationService.getCardType(doesNotStartwith51_55));
+
+        String doesNotStartwith51_55b = "5605105105105100";
+        Assertions.assertNotEquals(CardType.MasterCard, cardValidationService.getCardType(doesNotStartwith51_55b));
+
+        //non mastercard format - length < 16
+        String lengthLessThan13 = "55051051051051";
+        Assertions.assertNotEquals(CardType.MasterCard, cardValidationService.getCardType(lengthLessThan13));
+
+        //non mastercard format - length > 16
+        String lengthMoreThan16 = "550510510510510022";
+        Assertions.assertNotEquals(CardType.AMEX, cardValidationService.getCardType(lengthMoreThan16));
+
+        //contains non-numeric
+        String hasNonNumeric = "5505abc105105100";
+        Assertions.assertEquals(CardType.Unknown, cardValidationService.getCardType(hasNonNumeric));
+    }
+
+    @Test
+    void getCardType_validVisaFormat() {
         CreditCardValidationService cardValidationService = new CreditCardValidationServiceImpl();
 
         //valid visa format
@@ -38,7 +137,7 @@ class CreditCardValidationServiceImplTest {
         //non visa format - 13 > length < 16
         String lengthIs15 = "411111111111122";
         Assertions.assertNotEquals(CardType.Visa, cardValidationService.getCardType(lengthIs15));
-        //non visa format - length 16
+        //non visa format - length > 16
         String lengthMoreThan16 = "411111111111122222";
         Assertions.assertNotEquals(CardType.Visa, cardValidationService.getCardType(lengthMoreThan16));
 
